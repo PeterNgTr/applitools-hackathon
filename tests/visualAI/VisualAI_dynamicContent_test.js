fba1c3ed286232e8269b6d89f48d1250ebfc88c2
PeterNgTr <peter.nguyentr@gmail.com>
2019-11-15 11:48:00 +0100

    add condition to login if it is

--- tests/visualAI/VisualAI_dynamicContent_test.js
+++ tests/visualAI/VisualAI_dynamicContent_test.js
@@ -4,6 +4,11 @@ Feature('VisualAITests - Dynamic Content');
 
 Scenario('Flash sale gifs @visualTestDynamicContent', async () => {
     I.amOnPage(`${process.env['TARGET_URL']}?showAd=true`)
+    if (await I.grabNumberOfVisibleElements('#username') >= 1) {
+        I.fillField('#username', 'abc');
+        I.fillField('#password', 'abc');
+        I.click('#log-in');
+    }
     await I.eyeCheck('Dynamic Content', 'dynamicContent', 'Layout');
 });
 
